9d8f3e36dfd00e0c58d909f08897e3af6285d1a3
Bernard Decelian <bdecelia@andrew.cmu.edu>
2017-04-29 16:07:53 -0400

    added routes and placeholder files to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  
+
   # Routes for main resources
   resources :items
   resources :purchases
   resources :item_prices
+  resources :orders
+  resources :order_items
+  resources :schools
+  resources :users
 
   # Semi-static page routes
   get 'home' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
-  
+
   # Set the root url
-  root :to => 'home#home'  
+  root :to => 'home#home'
 
 end
