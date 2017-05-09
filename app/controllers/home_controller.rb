043b9199145ce5d3cf962076c3ad392122ae8746
Bernard Decelian <bdecelia@andrew.cmu.edu>
2017-05-09 01:11:42 -0400

    small changes to homepages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def home
-    @popular_items = Item.joins("INNER JOIN items ON items.id = O.item_id").from(OrderItem.select("item_id, COUNT(item_id) as count").group("item_id").order("count DESC").limit(10),:O)
-    @unshipped_orders = Order.not_shipped.chronological.to_a
-    @items_to_reorder = Item.need_reorder.alphabetical.to_a
-    @previously_bought = Item.where(:id => OrderItem.where(:order_id => Order.where(user_id: current_user.id).select("id")).select("item_id")).to_a if current_user
+    @popular_items = Item.joins("INNER JOIN items ON items.id = O.item_id").from(OrderItem.select("item_id, COUNT(item_id) as count").group("item_id").order("count DESC").limit(10),:O).paginate(:page => params[:page]).per_page(10)
+    @unshipped_orders = Order.not_shipped.chronological.paginate(:page => params[:page]).per_page(10)
+    @items_to_reorder = Item.need_reorder.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @previously_bought = Item.where(:id => OrderItem.where(:order_id => Order.where(user_id: current_user.id).select("id")).select("item_id")).paginate(:page => params[:page]).per_page(10) if current_user
   end
 
   def about
